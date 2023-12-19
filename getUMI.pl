f33f4289ada7c799d51f7be0d5f1a01c62a44ad5
Shuhuan Cao <caoshuhuan@yeah.net>
2023-12-19 09:31:07 +0800

    Update getUMI.pl

--- getUMI.pl
+++ getUMI.pl
@@ -1,11 +1,12 @@
-open IA,"gzip -dc $ARGV[0]|";
+open IA,"gzip -dc $ARGV[0]|" or die "$ARG[0] unexists\n";
 while(<IA>){
 	chomp;
 	my @F = split /\t/;
 	$info{$F[0]} = "$F[2]\t$F[1]";
 }
+close IA;
 
-open IB,"gzip -dc $ARGV[1]|";
+open IB,"gzip -dc $ARGV[1]|" or die "$ARGV[1] unexists\n";
 while(<IB>){
 	chomp;
 	my @F = split /\|\|\|/;
@@ -13,4 +14,4 @@ while(<IB>){
 	next unless (exists $info{$F[0]});
 	print "$info{$F[0]}\t$F[1]\t$F[2]\t$F[0]\n";
 }
-
+close IB;
