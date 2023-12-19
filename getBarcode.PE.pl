649d6a280a236f55d4f2b0ff63251b95b1ee4c05
Shuhuan Cao <caoshuhuan@yeah.net>
2023-12-19 09:28:39 +0800

    Update getBarcode.PE.pl

--- getBarcode.PE.pl
+++ getBarcode.PE.pl
@@ -1,7 +1,8 @@
-open IA,"gzip -dc $ARGV[0]|";
+open IA,"gzip -dc $ARGV[0]|" or die "$ARGV[0] unexists\n";
 while(<IA>){
 	chomp;
 	my @F = split /\t/;
 	next unless ($_ =~ /^@/);
 	print "$_\n";
 }
+close IA;
